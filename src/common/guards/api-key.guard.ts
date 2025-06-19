31de8aa0d11d26003a368e4b5dfefcc6a5a91778
cuongbq <erikbuidt@gmail.com>
2025-06-19 15:31:36 +0700

    fix: add logging for API key retrieval in ApiKeyGuard

--- src/common/guards/api-key.guard.ts
+++ src/common/guards/api-key.guard.ts
@@ -7,6 +7,8 @@ export class ApiKeyGuard implements CanActivate {
     const request = context.switchToHttp().getRequest()
     // biome-ignore lint/complexity/useLiteralKeys: <explanation>
     const apiKey = request.headers["apikey"]
+    console.log("API KEY from header", apiKey)
+    console.log(request.headers)
     if (!apiKey) {
       return false
     }
